82c666beb8c25e21bc26c89f9c2c0488a3bceb12
michellepae <michellepae75@gmail.com>
2026-04-01 09:58:52 +0900

    Update comments in InputTodo.vue for clarity

--- src/components/InputTodo.vue
+++ src/components/InputTodo.vue
@@ -18,16 +18,16 @@
 <script setup>
 import { ref } from 'vue';
 
-// 1. 이름 정의 (필요한 경우에만 작성, 보통 파일명이 이름이 됨)
+// 이름 정의 (필요한 경우에만 작성, 보통 파일명이 이름이 됨)
 // defineOptions({ name: 'InputTodo' }); 
 
-// 2. 반응형 데이터 선언 (data -> ref)
+// 반응형 데이터 선언 (data -> ref)
 const todo = ref('');
 
-// 3. Emit 정의 (emits 옵션 대용)
+//  Emit 정의 (emits 옵션 대용)
 const emit = defineEmits(['add-todo']);
 
-// 4. 핸들러 함수 구현 (methods -> function)
+// 핸들러 함수 구현 (methods -> function)
 const addTodoHandler = () => {
   if (todo.value.length >= 3) {
     // 부모에게 이벤트와 값 전달
